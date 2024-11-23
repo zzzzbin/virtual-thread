e475a936af996977aa8a4e0c2093b525fff08bd5
Trung Nguyen <trung.nguyen.spk@gmail.com>
2024-11-23 15:56:28 +0700

    virtual thread scaling

--- src/main/java/com/si/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/com/si/sec01/InboundOutboundTaskDemo.java
@@ -2,13 +2,20 @@ package com.si.sec01;
 
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * To demo some blocking operations with both platform and virtual threads
+ */
 public class InboundOutboundTaskDemo {
     private static final int MAX_PLATFORM = 10;
+    private static final int MAX_VIRTUAL = 200_000;
 
     public static void main(String[] args) throws InterruptedException {
-        platformThreadDemo3();
+        virtualThreadDemo();
     }
 
+    /**
+     * To create a simple java platform thread
+     */
     private static void platformThreadDemo1() {
         for (int i = 0; i < MAX_PLATFORM; i++) {
             int j = i;
@@ -17,6 +24,9 @@ public class InboundOutboundTaskDemo {
         }
     }
 
+    /**
+     To create platform thread using Thread.Builder
+     */
     private static void platformThreadDemo2() {
         var builder = Thread.ofPlatform().name("vins", 1);
         for (int i = 0; i < MAX_PLATFORM; i++) {
@@ -26,11 +36,31 @@ public class InboundOutboundTaskDemo {
         }
     }
 
-    //How to make our application wait for all thread finish before exit app -> CountDownLatch
+    /**
+     *  To create platform daemon thread using Thread.Builder
+     */
     private static void platformThreadDemo3() throws InterruptedException {
         var latch = new CountDownLatch(MAX_PLATFORM);
         var builder = Thread.ofPlatform().daemon().name("daemon", 1);
         for (int i = 0; i < MAX_PLATFORM; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(() -> {
+                Task.ioIntensive(j);
+                latch.countDown();
+            });
+            thread.start();
+        }
+        latch.await();
+    }
+
+    /**
+     * To create virtual thread using Thread.Builder
+     * - virtual threads are daemon thread by default
+     */
+    private static void virtualThreadDemo() throws InterruptedException {
+        var latch = new CountDownLatch(MAX_VIRTUAL);
+        var builder = Thread.ofVirtual();
+        for (int i = 0; i < MAX_VIRTUAL; i++) {
             int j = i;
             Thread thread = builder.unstarted(() -> {
                 Task.ioIntensive(j);
