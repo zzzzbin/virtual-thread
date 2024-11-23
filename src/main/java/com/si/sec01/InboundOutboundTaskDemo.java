fd3217f92f52d285c19d4250da5e58a593577970
Trung Nguyen <trung.nguyen.spk@gmail.com>
2024-11-23 15:37:58 +0700

    thread builder - factory method

--- src/main/java/com/si/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/com/si/sec01/InboundOutboundTaskDemo.java
@@ -1,17 +1,43 @@
 package com.si.sec01;
 
-public class InboundOutboundTaskDemo {
-    private static final int MAX_PLATFORM = 50_000;
+import java.util.concurrent.CountDownLatch;
 
-    public static void main(String[] args) {
-        platformThreadDemo();
+public class InboundOutboundTaskDemo {
+    private static final int MAX_PLATFORM = 10;
+
+    public static void main(String[] args) throws InterruptedException {
+        platformThreadDemo3();
     }
 
-    private static void platformThreadDemo(){
-        for (int i = 0; i < MAX_PLATFORM; i++){
+    private static void platformThreadDemo1() {
+        for (int i = 0; i < MAX_PLATFORM; i++) {
             int j = i;
-            Thread thread = new Thread(()->Task.ioIntensive(j));
+            Thread thread = new Thread(() -> Task.ioIntensive(j));
             thread.start();
         }
+    }
+
+    private static void platformThreadDemo2() {
+        var builder = Thread.ofPlatform().name("vins", 1);
+        for (int i = 0; i < MAX_PLATFORM; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(() -> Task.ioIntensive(j));
+            thread.start();
+        }
+    }
+
+    //How to make our application wait for all thread finish before exit app -> CountDownLatch
+    private static void platformThreadDemo3() throws InterruptedException {
+        var latch = new CountDownLatch(MAX_PLATFORM);
+        var builder = Thread.ofPlatform().daemon().name("daemon", 1);
+        for (int i = 0; i < MAX_PLATFORM; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(() -> {
+                Task.ioIntensive(j);
+                latch.countDown();
+            });
+            thread.start();
+        }
+        latch.await();
     }
 }
