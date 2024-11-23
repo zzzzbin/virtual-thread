b34cd1c99a046e07637419f449c49da054c5eef2
Trung Nguyen <trung.nguyen.spk@gmail.com>
2024-11-23 21:44:02 +0700

    tracing pin thread

---
+++ src/main/java/com/si/sec05/Lec01RaceCondition.java
@@ -0,0 +1,4 @@
+package com.si.sec05;
+
+public class Lec01RaceCondition {
+}
---
+++ src/main/java/com/si/sec05/Lec03SynchronizationWithIO.java
@@ -0,0 +1,46 @@
+package com.si.sec05;
+
+/*
+    Virtual Threads are indented for I/O tasks. This is a simple demo to show that race conditions are still applicable.
+ */
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.si.util.CommonUtils;
+
+public class Lec03RaceCondition {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec03RaceCondition.class);
+    private static final List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        demo(Thread.ofVirtual());
+
+        CommonUtils.sleep(Duration.ofSeconds(2));
+
+        log.info("list size: {}", list.size());
+    }
+
+    private static void demo(Thread.Builder builder){
+        for (int i = 0; i < 50; i++) {
+            builder.start(() -> {
+                log.info("Task started. {}", Thread.currentThread());
+                for (int j = 0; j < 200; j++) {
+                    inMemoryTask();
+                }
+                log.info("Task ended. {}", Thread.currentThread());
+            });
+        }
+    }
+
+    private static synchronized void inMemoryTask(){
+        list.add(1);
+    }
+
+}
