fdf4faa5f2ce6749ba0d73e3b4eae79d4a55be1d
Trung Nguyen <trung.nguyen.spk@gmail.com>
2024-12-07 15:47:17 +0700

    get product info

---
+++ src/main/java/com/si/sec08/Lec04GetProducts.java
@@ -0,0 +1,32 @@
+package com.si.sec08;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.si.sec07.externalservice.Client;
+
+/*
+    To get multiple products information in parallel
+ */
+public class Lec04GetProducts {
+    private static final Logger log = LoggerFactory.getLogger(Lec04GetProducts.class);
+
+    public static void main(String[] args) throws Exception {
+
+        try(var executor = Executors.newVirtualThreadPerTaskExecutor()){
+            var product1 = CompletableFuture.supplyAsync(() -> Client.getProduct(1), executor);
+            var product2 = CompletableFuture.supplyAsync(() -> Client.getProduct(2), executor);
+            var product3 = CompletableFuture.supplyAsync(() -> Client.getProduct(3), executor);
+
+            log.info("product-1: {}", product1.get());
+            log.info("product-2: {}", product2.get());
+            log.info("product-3: {}", product3.get());
+
+        }
+
+    }
+
+}
---
+++ src/main/java/com/si/sec08/externalservice/Client.java
@@ -0,0 +1,34 @@
+package com.si.sec08.externalservice;
+
+
+
+import java.net.URI;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Client {
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
+    private static final String PRODUCT_REQUEST_FORMAT = "http://localhost:7070/sec01/product/%d";
+    private static final String RATING_REQUEST_FORMAT = "http://localhost:7070/sec01/rating/%d";
+
+    public static String getProduct(int productId) {
+        return callExternalService(PRODUCT_REQUEST_FORMAT.formatted(productId));
+    }
+
+    public static Integer getRating(int productId) {
+        return Integer.parseInt(callExternalService(RATING_REQUEST_FORMAT.formatted(productId)));
+    }
+    private static String callExternalService(String url){
+        logger.info("Calling external service: {}", url);
+        try(var stream = URI.create(url).toURL().openStream()){ //stream should be closed
+            return new String(stream.readAllBytes());
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
