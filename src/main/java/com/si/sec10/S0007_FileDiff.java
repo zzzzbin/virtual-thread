9e6c8720c9267ff466837a0aa1ed8c4386aa3d15
Trung Nguyen <trung.nguyen.spk@gmail.com>
2024-12-09 11:13:35 +0700

    FileDiff

---
+++ src/main/java/com/si/sec10/S0007_FileDiff.java
@@ -0,0 +1,19 @@
+package com.si.sec10;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface S0007_FileDiff {
+    static final Logger log = LoggerFactory.getLogger(S0007_FileDiff.class);
+    static void main(String... args) throws IOException {
+        var first = Path.of("S0003_VarArgs.java");
+        var second = Path.of("S0004_Timer.java");
+        log.info(System.getProperty("user.dir"));
+        var firstDifferentByte = Files.mismatch(first, second);
+        System.out.println(firstDifferentByte);
+    }
+}
